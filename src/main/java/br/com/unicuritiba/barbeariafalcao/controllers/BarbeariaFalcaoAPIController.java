ef2c7f5b697f1137821022b94beb16a98588cadb
przybeuka <jeander.kauanp@gmail.com>
2024-06-06 00:38:37 -0300

    Descrição das modificações realizadas

--- src/main/java/br/com/unicuritiba/barbeariafalcao/controllers/BarbeariaFalcaoAPIController.java
+++ src/main/java/br/com/unicuritiba/barbeariafalcao/controllers/BarbeariaFalcaoAPIController.java
@@ -3,6 +3,7 @@ package br.com.unicuritiba.barbeariafalcao.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,55 +12,52 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.unicuritiba.barbeariafalcao.models.Agenda;
 import br.com.unicuritiba.barbeariafalcao.repositories.AgendaRepository;
 
-@RestController
+
+@Controller
 public class BarbeariaFalcaoAPIController {
 
     @Autowired
     private AgendaRepository repository;
 
-	@GetMapping("/") public ModelAndView home() {
-		
-		List<Agenda> agenda = repository.findAll();
-		
-		ModelAndView view = new ModelAndView("index");
-		view.addObject("agenda", agenda);
-		
-		return view;
-	}
-	
-	@GetMapping("/agendar") public ModelAndView agendar() {	
-		
-		ModelAndView view = new ModelAndView("agendar");
-		view.addObject("model", new Agenda());
-		
-		return view;
-	}
-	
-	@GetMapping("/atualizar") public ModelAndView atualizar() {	
-		
-		List<Agenda> agenda = repository.findAll();
-		
-		ModelAndView view = new ModelAndView("atualizar");
-		view.addObject("agenda", agenda);
-		
-		return view;
-	}
-	
-	@PostMapping("/agendar")
+    //MOSTRA LISTA DA AGENDA
+    @GetMapping("/") 
+    public ModelAndView home() {
+        List<Agenda> agenda = repository.findAll();
+        ModelAndView view = new ModelAndView("index");
+        view.addObject("agenda", agenda);
+        return view;
+    }
+   
+    //MOSTRAR PAGINA AGENDAR
+    @GetMapping("/agendar") 
+    public ModelAndView agendar() {    
+        ModelAndView view = new ModelAndView("agendar");
+        view.addObject("model", new Agenda());
+        return view;
+    }
+    
+    //SALVA NA AGENDA
+    @PostMapping("/agendar")
     public String salvarHorario(Agenda agenda, Model model, BindingResult result) {
-
-		repository.save(agenda);
-
+        repository.save(agenda);
         return "redirect:/";
     }
-	
-	
+    
+    //MOSTRAR PAGINA ATUALIZAR
+    @GetMapping("/atualizar") 
+    public ModelAndView atualizar() {    
+        List<Agenda> agenda = repository.findAll();
+        ModelAndView view = new ModelAndView("atualizar");
+        view.addObject("agenda", agenda);
+        return view;
+    }
+    
+    //PEGA PELO ID O CLIENTE NA AGENDA E DIRECIONA PARA REMARCAR
     @GetMapping("/remarcar/{id}")
     public ModelAndView atualizarCliente(@PathVariable("id") long id) {
         Agenda agenda = repository.findById(id).orElseThrow(() -> new IllegalArgumentException("ID inválido: " + id));
@@ -67,34 +65,37 @@ public class BarbeariaFalcaoAPIController {
         view.addObject("agenda", agenda);
         return view;
     }
-	
     
+    //REMARCA NA AGENDA PELO ID O CLIENTE JA SELECIONADO
     @PutMapping("/remarcar/{id}")
     public String remarcarCliente(@PathVariable("id") long id, @ModelAttribute Agenda agenda, Model model) {
-        // Recuperar o agendamento existente do banco de dados
         Agenda agendamentoExistente = repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("ID inválido: " + id));
-        
-        // Atualizar os detalhes do agendamento com os novos dados do formulário
+
         agendamentoExistente.setNome(agenda.getNome());
         agendamentoExistente.setTelefone(agenda.getTelefone());
         agendamentoExistente.setDia(agenda.getDia());
         agendamentoExistente.setHorario(agenda.getHorario());
         agendamentoExistente.setBarbeiro(agenda.getBarbeiro());
         agendamentoExistente.setServico(agenda.getServico());
-        
-        // Salvar as alterações no banco de dados
+
         repository.save(agendamentoExistente);
-        
-        // Redirecionar para a página inicial ou para onde desejar após a atualização
+
         return "redirect:/";
     }
+    
+    //DELETA O AGENDAMENTO PELO ID DA TABELA
+    @DeleteMapping("/{id}")
+    public String feito(@PathVariable("id") long id) {
+        repository.deleteById(id);
+        return "redirect:/"; 
+    }
 
-	
-	@DeleteMapping("/remover/{id}")
-	public String removeAgenda(@PathVariable("id") long id) {
-		repository.deleteById(id);
-	    return "redirect:/"; 
-	}
-	
+    
+    //DELETA O AGENDAMENTO PELO ID DA TABELA
+    @DeleteMapping("/remover/{id}")
+    public String removeAgenda(@PathVariable("id") long id) {
+        repository.deleteById(id);
+        return "redirect:/"; 
+    }
 }
